69f45245ad28fea4787135b68abe2e163a2b4b6a
Muhaiminul-Islam-Kanon <mikanon37@gmail.com>
2026-07-02 23:20:35 +0600

    Test case added:v2

--- test_calculator.py
+++ test_calculator.py
@@ -15,6 +15,7 @@ def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(-2, 3) == -6
     assert multiply(0, 5) == 0
+    assert multiply(5, 3) == 2
 
 def test_divide():
     assert divide(6, 3) == 2
