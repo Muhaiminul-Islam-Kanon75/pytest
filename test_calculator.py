0d045000629f36de1c60f630418ea866d1615034
Muhaiminul-Islam-Kanon <mikanon37@gmail.com>
2026-07-02 23:26:27 +0600

    test case added:v3

--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,7 @@ def test_add():
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 5) == -5
+    assert subtract(12, 5) == 7
 
 def test_multiply():
     assert multiply(2, 3) == 6
